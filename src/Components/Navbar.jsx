dfb0edd0371dc780bc305e52e4030ab60028e544
Devasya <devasyarajguru@gmail.com>
2024-01-29 19:40:37 +0530

    feat: Creating the Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,32 @@
 // Navbar.js
+import Logo2 from "../assets/image2.png"
+
 
 import { Link } from 'react-router-dom';
 import '../CSS/Navbar.css';
 
-const Navbar = () => {
-    
+
+import PropTypes from 'prop-types'; 
+import { useEffect } from 'react';
+
+const Navbar = (props) => {
+  Navbar.propTypes =
+  {
+    animateCircle:PropTypes.bool.isRequired
+  }
+
+  useEffect(() =>
+  {
+    if(props.animateCircle)
+    {
+      document.querySelector('.navbar').classList.add('show-navbar')
+    }
+  },[props.animateCircle])
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${props.animateCircle ? 'show-navbar' : ""}`}>
+      <div className='logo-container'>
+          <img src={Logo2} alt="Logo1" className="logo"/>
+        </div>
       <ul className="nav-list">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/contact">Contact</Link></li>
