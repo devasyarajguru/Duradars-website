4aa3ec88ca7f5403f31b8fc1a4f8c6639f4f1efa
Devasya <devasyarajguru@gmail.com>
2024-02-02 22:29:54 +0530

    Why Section Updated

--- src/Components/Why.jsx
+++ src/Components/Why.jsx
@@ -4,47 +4,63 @@ import { MdOutlineSupportAgent } from "react-icons/md";
 import { AiOutlineSolution } from "react-icons/ai";
 import { GrUserExpert } from "react-icons/gr";
 import { BsPersonWorkspace } from "react-icons/bs";
+import { Fade } from 'react-reveal';
 
 const Why = () => {
+  const WhyDetails = 
+  {
+    mainTitle:"Why Choose Us?",
+    items:
+    [
+      {
+        title:"24/7 Support",
+        icon:<MdOutlineSupportAgent className='support'/>,
+        paragraph:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus libero ducimus, velit qui asperiores culpa, totam voluptas ipsum minima magnam, mollitia odio ad ea expedita nobis dolorem illum aliquam fugiat!"
+      },
+      {
+        title:"Customized Solutions",
+        icon: <AiOutlineSolution  className='support'/>,
+        paragraph:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus libero ducimus, velit qui asperiores culpa, totam voluptas ipsum minima magnam, mollitia odio ad ea expedita nobis dolorem illum aliquam fugiat!"
+      },
+      {
+        title:"Experience and Expertise",
+        icon:<GrUserExpert className='support'/>,
+        paragraph:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus libero ducimus, velit qui asperiores culpa, totam voluptas ipsum minima magnam, mollitia odio ad ea expedita nobis dolorem illum aliquam fugiat!"
+      },
+      {
+        title:"Proactive Approach",
+        icon:<BsPersonWorkspace  className='support'/>,
+        paragraph:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus libero ducimus, velit qui asperiores culpa, totam voluptas ipsum minima magnam, mollitia odio ad ea expedita nobis dolorem illum aliquam fugiat!"
+      },
+    ]
+  }
   return (
-    <div className="why-container">
-      <h2>Why Choose Us?</h2>
-
+    <Fade bottom distance="30%"  duration={2500}>
+      {/* Why Container */}
+    <section className="why-container">
+      <h2>{WhyDetails.mainTitle}</h2>
+        {/* Why Grid Starts*/}
       <div className="why-grid">
-        <div className="why-item">
-        <MdOutlineSupportAgent className='support'/>
-          <p className='why-first'>
-            24/7 Support
-          </p>
-          <p className="why-second">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus libero ducimus, velit qui asperiores culpa, totam voluptas ipsum minima magnam, mollitia odio ad ea expedita nobis dolorem illum aliquam fugiat!</p>
+        {/* Items */}
+          {WhyDetails.items.map((detail) =>
+          (
+            <div className="why-item" key={detail.title}>
+            {detail.icon}
+            <p className='why-first'>
+              {detail.title}
+            </p>
+            <p className="why-second">{detail.paragraph}</p>
         </div>
-
-        <div className="why-item">
-        <AiOutlineSolution  className='support'/>
-          <p className='why-first'>
-          Customized Solutions
-          </p>
-          <p className="why-second">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus libero ducimus, velit qui asperiores culpa, totam voluptas ipsum minima magnam, mollitia odio ad ea expedita nobis dolorem illum aliquam fugiat!</p>
-        </div>
-
-        <div className="why-item">
-        <GrUserExpert className='support'/>
-          <p className='why-first'>
-          Experience and Expertise
-          </p>
-          <p className="why-second">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus libero ducimus, velit qui asperiores culpa, totam voluptas ipsum minima magnam, mollitia odio ad ea expedita nobis dolorem illum aliquam fugiat!</p>
-        </div>
-
-        <div className="why-item">
-        <BsPersonWorkspace  className='support'/>
-          <p className='why-first'>
-          Proactive Approach
-          </p>
-          <p className="why-second">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus libero ducimus, velit qui asperiores culpa, totam voluptas ipsum minima magnam, mollitia odio ad ea expedita nobis dolorem illum aliquam fugiat!</p>
-        </div>
+          ))}
+        {/* Items */}
 
       </div>
-    </div>
+        {/* Why Grid Ends*/}
+
+    </section>
+      {/* Why Container Ends*/}
+
+    </Fade>
   );
 };
 
