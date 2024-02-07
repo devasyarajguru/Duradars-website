10907d5245270ea7982e7dc8f8752aafe86bdb39
Devasya <devasyarajguru@gmail.com>
2024-02-07 21:06:40 +0530

    Updating Home Component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,43 +2,20 @@ import PropTypes from "prop-types";
 import AboutHome from './AboutSec';
 import ServiceSec from './ServiceSec';
 import Why from './Why';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import '../CSS/Home.css'
+import MainHero from "./MainHero";
+
 const Home = ({taglineJSX, taglineText , paragraph , image}) =>
 {
     
     return(
-       // Main Home Container
        <>
-        <section className='main-home-container'>
-         {/* Tagline and Paragraph */}
-            {/* Content Container starts */}
-            <div className='content-container-wrapper'>
-                <div className="content-container fade-in">
-                  
-                    {/*Hero Section content Starts */}
-                    <div className="hero-section-content">
-                    <h2 className="tagline slide-left"> {taglineJSX}
-                {taglineText}</h2>
-                    <p className="paragraph slide-left">{paragraph}</p>
-                    <Link to="/contact">
-                      <button className="custom-button slide-left">Contact Us</button>
-                    </Link>
-                    </div>
-                    {/*Hero Section content Ends */}
+        {/* MainHero Content Starts*/}
 
-                    <div className="hero-section-image">
-                    {/*Hero Section Image Starts */}
-                      <div className='hero-image'>
-                        <img src={image} alt="Cyber-Image" className="svg-image slide-right"/>
-                      </div>
-                    </div>
-                    {/* Hero Section Image ends */}
-                    </div>
-                </div>
-            {/* Content Container Ends */}
-      </section>
-      {/* Main Home Container Ends */}
+        {MainHero({ taglineJSX, taglineText, paragraph, image })}
+          
+          {/* MainHero Content Ends*/}
 
       <div>
           <AboutHome />
