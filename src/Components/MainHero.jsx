0b173e83c4c024e5d13381e735d3b1de41fdbcd7
Devasya <devasyarajguru@gmail.com>
2024-02-07 21:12:56 +0530

    Main Component to be used in different section of pages

---
+++ src/Components/MainHero.jsx
@@ -0,0 +1,48 @@
+// Helper function for rendering the main home container
+import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+
+const MainHero = ({ taglineJSX, taglineText, paragraph, image }) => (
+    <section className='main-home-container'>
+      {/* Tagline and Paragraph */}
+      {/* Content Container starts */}
+      <div className='content-container-wrapper'>
+        <div className="content-container fade-in">
+          {/* Hero Section content Starts */}
+          <div className="hero-section-content">
+            <h2 className="tagline slide-left">{taglineJSX}{taglineText}</h2>
+            <p className="paragraph slide-left">{paragraph}</p>
+            <Link to="/contact">
+              <button className="custom-button slide-left">Contact Us</button>
+            </Link>
+          </div>
+          {/* Hero Section content Ends */}
+  
+          <div className="hero-section-image">
+            {/* Hero Section Image Starts */}
+            <div className='hero-image'>
+              <img src={image} alt="Cyber-Image" className="svg-image slide-right"/>
+            </div>
+          </div>
+          {/* Hero Section Image ends */}
+        </div>
+      </div>
+      {/* Content Container Ends */}
+    </section>
+  );
+  MainHero.defaultProps = {
+    taglineJSX: null,
+    taglineText: "",
+    paragraph: "",
+    image: "",
+  };
+  
+  MainHero.propTypes = {
+    taglineJSX: PropTypes.node,
+    taglineText: PropTypes.string.isRequired,
+    paragraph: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  };
+  // Usage in your pages (e.g., AboutUs.jsx, Services.jsx)
+ export default MainHero;
+  
