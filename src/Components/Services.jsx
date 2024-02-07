aa9e41192e6d213b2acce93c3519e96a37d5d10f
Devasya <devasyarajguru@gmail.com>
2024-02-07 21:14:11 +0530

    Services page updated

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,161 +1,82 @@
 import PropTypes from "prop-types";
-import { Link } from 'react-router-dom';
 import '../CSS/Home.css'
 import '../CSS/Services.css'
 import { Fade } from "react-reveal";
+import { MdSecurity } from "react-icons/md";
+import MainHero from "./MainHero";
+import Why from "./Why";
+import ContactSec from "./ContactSec";
 
 const Services = ({taglineJSX, taglineText , paragraph , image}) =>
 {
-    // const services = [
-    //     {
-    //       title: 'Web Application',
-    //       icon: 'fas fa-layer-group',
-    //       description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...',
-    //     },
-    //     {
-    //       title: 'Highly customizable',
-    //       icon: 'far fa-chart-bar',
-    //       description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...',
-    //     },
-    //     {
-    //       title: 'Responsive design',
-    //       icon: 'fas fa-database',
-    //       description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...',
-    //     },
-    //     {
-    //       title: 'Service & Plugins',
-    //       icon: 'fas fa-cogs',
-    //       description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...',
-    //     },
-    //     {
-    //       title: 'Optimized for speed',
-    //       icon: 'fas fa-chart-pie',
-    //       description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...',
-    //     },
-    //     {
-    //       title: 'Dedicated support',
-    //       icon: 'fas fa-thumbs-up',
-    //       description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...',
-    //     },
-    //   ];
+  const serviceDetails = [
+    {
+      icon: <MdSecurity />,
+      heading: 'Web PenTesting',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.',
+    },
+    {
+      icon: <MdSecurity />,
+      heading: 'Application PenTesting',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.',
+    },
+    {
+      icon: <MdSecurity />,
+      heading: 'Network Security',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.',
+    },
+    {
+      icon: <MdSecurity />,
+      heading: 'Cloud Security',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.',
+    },
+    {
+      icon: <MdSecurity />,
+      heading: 'WebDev with Security',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.',
+    },
+    {
+      icon: <MdSecurity />,
+      heading: 'Source Code Review',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.',
+    },
+  ];
 
     return(
-        // Main About Us Container
+        
        <>
-       <section className='main-home-container'>
-        {/* Tagline and Paragraph */}
-           {/* Content Container starts */}
-           <div className='content-container-wrapper'>
-               <div className="content-container fade-in">
-                 
-                   {/*Hero Section content Starts */}
-                   <div className="hero-section-content">
-                   <h2 className="tagline slide-left"> {taglineJSX}{taglineText}</h2>
-                   <p className="paragraph slide-left">{paragraph}</p>
-                   <Link to="/contact">
-                     <button className="custom-button slide-left">Contact Us</button>
-                   </Link>
-                   </div>
-                   {/*Hero Section content Ends */}
+        {/* MainHero Content Starts*/}
+        {MainHero({ taglineJSX, taglineText, paragraph, image })}
+         {/* MainHero Content Ends*/}
 
-                   <div className="hero-section-image">
-                   {/*Hero Section Image Starts */}
-                     <div className='hero-image'>
-                       <img src={image} alt="Cyber-Image" className="svg-image slide-right"/>
-                     </div>
-                   </div>
-                   {/* Hero Section Image ends */}
-                   </div>
-               </div>
-           {/* Content Container Ends */}
-       
        {/* Responsive Container Starts*/}
-       <Fade bottom distance="30%" duration={1500}>
+       <Fade bottom distance="30%" duration={2500}>
        
-    <section id="responsive">
- 
- <div className="row">
-   <div className="column">
-     <div className="card">
-       <div className="icon-wrapper">
-         <i className="fas fa-hammer"></i>
-       </div>
-       <h3>Service Heading</h3>
-       <p>
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-         consequatur necessitatibus eaque.
-       </p>
-     </div>
-   </div>
-   <div className="column">
-     <div className="card">
-       <div className="icon-wrapper">
-         <i className="fas fa-brush"></i>
-       </div>
-       <h3>Service Heading</h3>
-       <p>
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-         consequatur necessitatibus eaque.
-       </p>
-     </div>
-   </div>
-   <div className="column">
-     <div className="card">
-       <div className="icon-wrapper">
-         <i className="fas fa-wrench"></i>
-       </div>
-       <h3>Service Heading</h3>
-       <p>
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-         consequatur necessitatibus eaque.
-       </p>
-     </div>
-   </div>
-   <div className="column">
-     <div className="card">
-       <div className="icon-wrapper">
-         <i className="fas fa-truck-pickup"></i>
-       </div>
-       <h3>Service Heading</h3>
-       <p>
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-         consequatur necessitatibus eaque.
-       </p>
-     </div>
-   </div>
-   <div className="column">
-     <div className="card">
-       <div className="icon-wrapper">
-         <i className="fas fa-broom"></i>
-       </div>
-       <h3>Service Heading</h3>
-       <p>
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-         consequatur necessitatibus eaque.
-       </p>
-     </div>
-   </div>
-   <div className="column">
-     <div className="card">
-       <div className="icon-wrapper">
-         <i className="fas fa-plug"></i>
-       </div>
-       <h3>Service Heading</h3>
-       <p>
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-         consequatur necessitatibus eaque.
-       </p>
-     </div>
-   </div>
- </div>
-</section>
+       <section id="responsive">
+      <h2>Our Services</h2>
+      <div className="row">
+        {serviceDetails.map((service, index) => (
+          <div key={index} className="column">
+            <div className="card">
+              <div className="icon-wrapper">
+                {service.icon}
+              </div>
+              <h3 id="heading">{service.heading}</h3>
+              <p className="card-para">{service.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
 
 
     </Fade>
        {/* Responsive Container Ends*/}
 
-     </section>
-     {/* Main About Us Container Ends */}
+       <div>
+        <Why />
+        <ContactSec />
+       </div>
      </>
     )
 }
