a827723610673b79e1e2bfccfbea92c6d5a22387
Devasya <devasyarajguru@gmail.com>
2024-03-19 14:17:31 +0530

    Navbar component updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,14 @@
 import Logo2 from "../assets/image2.png"
 import { Link } from 'react-router-dom';
 import '../CSS/Navbar.css';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  }
   return (
     <nav className="navbar">
       <div className='logo-container'>
@@ -11,7 +17,13 @@ const Navbar = () => {
           <img src={Logo2} alt="Logo2" className="logo"/>
       </Link>
         </div>
-      <ul className="nav-list">
+        {/* Render hamburger icon based on isOpen state */}
+      <div className={`menu-toggle ${isOpen ? 'active' : ''}`} onClick={handleToggle}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+        <ul className={`nav-list ${isOpen ? 'active' : ''}`}>
       <li><Link to="/#main-home-container">Home</Link></li>
         <li><Link to="/services#main-home-container">Services</Link></li>
         <li><Link to="/aboutus#main-home-container">About Us</Link></li>
