b808374163aca4a446abb60ed3c7935c993f2200
Devasya <devasyarajguru@gmail.com>
2024-03-20 14:56:49 +0530

    Merge Ved Panchal's changes with stashed changes

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,76 +1,82 @@
+import PropTypes from "prop-types";
+import '../CSS/Home.css'
+import '../CSS/About.css'
+import { Fade } from "react-reveal";
+import MainHero from "./MainHero";
+// import AboutSec from "./AboutSec";
+import ContactSec from "./ContactSec";
+import Why from "./Why";
 
-import PropTypes from 'prop-types';
-import Fade from 'react-reveal/Fade';
-import MainHero from './MainHero';
-import Why from './Why';
-import ContactSec from './ContactSec';
+const AboutUs = ({ taglineJSX, taglineText, paragraph, image }) =>
+{
+    const sectionDetails = [
+        {
+          imgSrc: 'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eaboutus1.svg',
+          heading: 'Our Mission',
+          subHeading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut.',
+        },
+        {
+          imgSrc: 'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/xpraup2.svg',
+          heading: 'Our Vision',
+          subHeading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut.',
+        },
+      ];
 
+    return(
+       <>
+          {/* MainHero Content Starts*/}
 
-const AboutUs = ({ taglineJSX, taglineText, paragraph, image }) => {
+          {MainHero({ taglineJSX, taglineText, paragraph, image })}
+          
+          {/* MainHero Content Ends*/}
 
-  const sectionDetails = [
-    {
-      imgSrc: 'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eaboutus1.svg',
-      heading: 'Our Mission',
-      subHeading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut.',
-    },
-    {
-      imgSrc: 'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/xpraup2.svg',
-      heading: 'Our Vision',
-      subHeading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut.',
-    },
-  ];
-
-  return (
-    <>
-      
-      {/* MainHero Content Starts */}
-      {MainHero({ taglineJSX, taglineText, paragraph, image })}
-      {/* MainHero Content Ends */}
-
-      {/* Responsive Container Starts */}
-      <div className="responsive-container-block bigContainer">
-        {sectionDetails.map((section, index) => (
-          <Fade key={index} bottom distance="30%" duration={2500}>
-            <div className={`responsive-container-block Container ${index === 1 ? 'bottomContainer' : ''}`}>
-              <img className="mainImg" src={section.imgSrc} alt={`Section ${index + 1}`} />
-              <div className="allText aboveText">
-                <p className="text-blk headingText">{section.heading}</p>
-                <p className="text-blk subHeadingText">{section.subHeading}</p>
-                <p className="text-blk description">{section.description}</p>
-              </div>
+        {/* <div>
+          <AboutSec />
+        </div> */}
+        {/* Responsive Container Starts*/}
+        <div className="responsive-container-block bigContainer">
+      {sectionDetails.map((section, index) => (
+        <Fade key={index} bottom distance="30%" duration={2500}>
+          <div className={`responsive-container-block Container ${index === 1 ? 'bottomContainer' : ''}`}>
+            <img className="mainImg" src={section.imgSrc} alt={`Section ${index + 1}`} />
+            <div className="allText aboveText">
+              <p className="text-blk headingText">{section.heading}</p>
+              <p className="text-blk subHeadingText">{section.subHeading}</p>
+              <p className="text-blk description">{section.description}</p>
             </div>
-          </Fade>
-        ))}
-      </div>
-      {/* Responsive Container */}
+          </div>
+        </Fade>
+      ))}
+    </div>
+        {/* Responsive Container */}
+        <div className="mainSec-div">
 
-      <div className="mainSec-div">
-        <div style={{ marginBottom: '350px', marginTop: '50px' }}>
+       <div style={{marginBottom:"350px", marginTop:"50px"}}>
           <Why />
         </div>
-        <div style={{ marginTop: '50px' }}>
+
+        <div style={{ marginTop:"50px"}}>
           <ContactSec />
         </div>
-      </div>
-    </>
-  );
-};
 
-AboutUs.defaultProps = {
-  taglineJSX: null,
-  taglineText: '',
-  paragraph: '',
-  image: '',
-};
+       </div>
+        </>
+      )}
 
-AboutUs.propTypes = {
-  taglineJSX: PropTypes.node,
-  taglineText: PropTypes.string.isRequired,
-  paragraph: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-};
+      AboutUs.defaultProps = {
+        taglineJSX: null,
+        taglineText: "",
+        paragraph: "",
+        image: "",
+      };
+      
+      AboutUs.propTypes = {
+        taglineJSX: PropTypes.node,
+        taglineText: PropTypes.string.isRequired,
+        paragraph: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+      };
 
 export default AboutUs;
