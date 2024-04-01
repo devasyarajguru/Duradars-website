0f549edeac45a70670b9e324766bfb32e2096193
Devasya <devasyarajguru@gmail.com>
2024-04-01 18:57:24 +0530

    chore:Update the images extension format

--- src/Components/AboutSec.jsx
+++ src/Components/AboutSec.jsx
@@ -1,8 +1,8 @@
 import Fade from 'react-reveal/Fade';
 import '../CSS/AboutSec.css'
 import { Link } from 'react-router-dom';
-import About1 from "../assets/about1.jpg"
-import About2 from "../assets/about2.jpg"
+import About1 from "../assets/about1.webp"
+import About2 from "../assets/about2.webp"
 import About3 from "../assets/about3.jpg"
 
 const AboutSec = () => {
