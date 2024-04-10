28c9e993a6e434cb7d3820ebf9dac28f112ac5ec
Devasya <devasyarajguru@gmail.com>
2024-04-10 16:47:47 +0530

    feat: Optimized images for About section

--- src/Components/AboutSec.jsx
+++ src/Components/AboutSec.jsx
@@ -17,32 +17,48 @@ const AboutSec = () => {
       <div className="about-us-container">
         {/* Image Stack Container */}
       <div className="image-stack">
+        <picture>
+          < source srcSet={`${SmallAbout1} 300w, ${MediumAbout1} 600w, ${LargeAbout1} 1200w`}
+            sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 30vw"/>
         <img
             src={SmallAbout1}
-            srcSet={`${SmallAbout1} 300w, ${MediumAbout1} 600w, ${LargeAbout1} 1200w`}
-            sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 30vw"
             alt="About Image 1"
             loading="lazy"
             className='about1-image'
+            width={300}
+            height={200}
         />
-       <img
+        </picture>
+        
+        <picture>
+          <source  srcSet={`${SmallAbout2} 300w, ${MediumAbout2} 600w, ${LargeAbout2} 1200w`}
+            sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 30vw"/>
+          <img
             src={SmallAbout2}
-            srcSet={`${SmallAbout2} 300w, ${MediumAbout2} 600w, ${LargeAbout2} 1200w`}
-            sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 30vw"
             alt="About Image 2"
             loading="lazy"
             className='about2-image'
+            width={300}
+            height={200}
           />
+        </picture>
+       
       </div>
         {/* Image Stack Container ends*/}
-      <img
-            src={SmallAbout3}
+        <picture>
+          <source
             srcSet={`${SmallAbout3} 300w, ${MediumAbout3} 600w, ${LargeAbout3} 1200w`}
-            sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 30vw"
+            sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 30vw"/>
+        <img
+            src={SmallAbout3}
             alt="About Image 3"
             loading="lazy"
             className="side-image"
+            width={300}
+            height={200}
           />
+        </picture>
+      
 
       {/* About Us Content */}
       <div className="about-us-content">
