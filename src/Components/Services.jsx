83d219486833779d6083c2c662848441a6c0376e
Devasya <devasyarajguru@gmail.com>
2024-04-22 18:58:19 +0530

    chore:Paragraph changed

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -15,37 +15,37 @@ const Services = ({ taglineJSX, taglineText, paragraph, image, smallImage , medi
       icon: <MdSecurity />,
       heading: "Web PenTesting",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.",
+        "Protect your web applications with our comprehensive penetration testing services. Our expert team identifies and mitigates vulnerabilities, ensuring the security of your online assets.",
     },
     {
       icon: <MdSecurity />,
       heading: "Application PenTesting",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.",
+        "Identify and address security weaknesses in your applications with our penetration testing services. We help fortify your software against cyber threats, safeguarding your data and reputation.",
     },
     {
       icon: <MdSecurity />,
       heading: "Network Security",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.",
+        "Secure your network infrastructure with proactive monitoring and threat detection. Our solutions protect against unauthorized access and data breaches, keeping your systems resilient.",
     },
     {
       icon: <MdSecurity />,
       heading: "Cloud Security",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.",
+        "Ensure the security of your cloud environment with our comprehensive solutions. From access controls to compliance assessments, we help you navigate cloud security challenges.",
     },
     {
       icon: <MdSecurity />,
       heading: "WebDev with Security",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.",
+        "Integrate security best practices into your web development process. Our team collaborates to build secure, resilient web applications from the ground up.",
     },
     {
       icon: <MdSecurity />,
       heading: "Source Code Review",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.",
+        "Ensure the integrity of your software applications with our thorough code reviews. Our analysts identify and remediate potential security risks in your codebase.",
     },
   ];
 
