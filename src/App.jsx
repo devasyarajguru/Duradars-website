9be950bc7fc299ed5e49d4434c2380098a965b33
Devasya <devasyarajguru@gmail.com>
2024-05-24 15:47:44 +0530

    Changing cloudinary URLs

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
       taglineJSX: <span className="span-head">Securing</span>,
       taglineText: " your peace of mind, one click 👆 at a time",
       paragraph: "Welcome to Duradars, your trusted partner in cybersecurity. Explore our services and empower your online presence with confidence.",
-      image: "https://res.cloudinary.com/dgn53hdci/image/upload/v1716460494/end-hero1_ocxlos.jpg",
+      image: "https://res.cloudinary.com/dgn53hdci/image/upload/v1716544889/Duradars/end-hero1_bxl36y.png",
       // smallImage:SmallHero4,
       // mediumImage:MediumHero4,
       // largeImage:LargeHero4
@@ -39,7 +39,7 @@ const App = () => {
       taglineJSX: <span className="span-head">Empowering Security</span>,
       taglineText: " in a Connected World",
       paragraph: "At Duradars, we empower businesses and individuals to navigate the digital landscape securely. Trust us to safeguard your digital journey.",
-      image: "https://res.cloudinary.com/dgn53hdci/image/upload/v1716460494/end-hero2_u84ieo.jpg",
+      image: "https://res.cloudinary.com/dgn53hdci/image/upload/v1716544891/Duradars/end-hero3_mgrcuc.png",
       // smallImage:SmallHero2,
       // mediumImage:MediumHero2,
       // largeImage:LargeHero2
@@ -49,7 +49,7 @@ const App = () => {
       taglineJSX: <span className="span-head">Comprehensive Security</span>,
       taglineText: " Solutions for Every Need",
       paragraph: "Duradars offers a wide range of security services tailored to meet your needs. Explore our services and take the first step towards a secure future.",
-      image: "https://res.cloudinary.com/dgn53hdci/image/upload/v1716460493/end-hero3_you2je.jpg",
+      image: "https://res.cloudinary.com/dgn53hdci/image/upload/v1716545648/Duradars/last-hero4_n2ahot.png",
       // smallImage:SmallHero3,
       // mediumImage:MediumHero3,
       // largeImage:LargeHero3
@@ -59,7 +59,7 @@ const App = () => {
       taglineJSX: <span className="span-head">Streamlined Security</span>,
       taglineText: " Processes for  Protection",
       paragraph: "Our approach to cybersecurity is built on robust processes and methodologies. Discover our process and experience peace of mind.",
-      image: "https://res.cloudinary.com/dgn53hdci/image/upload/v1716460493/end-hero4_sp1lyu.jpg",
+      image: "https://res.cloudinary.com/dgn53hdci/image/upload/v1716545642/Duradars/last-hero1_uecntq.png",
       // smallImage:SmallHero,
       // mediumImage:MediumHero,
       // largeImage:LargeHero
@@ -69,7 +69,7 @@ const App = () => {
       taglineJSX: <span className="span-head">Join Our Team</span>,
       taglineText: " and Shape the Future of Cybersecurity",
       paragraph: "Looking for an exciting career opportunity in cybersecurity? Join our dynamic team and work on cutting-edge projects that challenge and inspire you.",
-      image: "https://res.cloudinary.com/dgn53hdci/image/upload/v1716460494/end-hero5_jrv7la.jpg",
+      image: "https://res.cloudinary.com/dgn53hdci/image/upload/v1716545645/Duradars/last-hero2_vtmipc.png",
       // smallImage:SmallHero5,
       // mediumImage:MediumHero5,
       // largeImage:LargeHero5
@@ -79,7 +79,7 @@ const App = () => {
       taglineJSX: <span className="span-head">Get in Touch</span>,
       taglineText: " and Secure Your Future Today",
       paragraph: "Contact Duradars today and let us help you safeguard your digital assets. Reach out to us and secure your future with Duradars.",
-      image:"https://res.cloudinary.com/dgn53hdci/image/upload/v1716460494/end-hero6_kycf1f.jpg",
+      image:"https://res.cloudinary.com/dgn53hdci/image/upload/v1716545646/Duradars/last-hero3_gfwxrf.png",
       // smallImage:SmallHero6,
       // mediumImage:MediumHero6,
       // largeImage:LargeHero6
