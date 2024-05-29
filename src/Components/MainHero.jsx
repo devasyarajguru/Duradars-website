37ea857d77dc76e665c3a5e2cc4d73ba7cf055ca
Devasya <devasyarajguru@gmail.com>
2024-05-29 18:31:23 +0530

    feat:Using React cloudinary plugin

--- src/Components/MainHero.jsx
+++ src/Components/MainHero.jsx
@@ -4,7 +4,8 @@ import PropTypes from "prop-types";
 import { useEffect, useRef } from "react";
 import { Cloudinary } from '@cloudinary/url-gen'; // Importing from cloudinary
 import { AdvancedImage } from '@cloudinary/react';
-import { scale } from "@cloudinary/url-gen/actions/resize";
+import { responsive } from '@cloudinary/react'
+// import { scale } from "@cloudinary/url-gen/actions/resize";
 
 const MainHero = ({
   taglineJSX,
@@ -16,14 +17,16 @@ const MainHero = ({
   useEffect(() => {
     topRef.current.scrollIntoView({ behavior: "smooth" });
     // Logging the current image source on resize
-    // const img =  document.querySelector(".svg-image");
-    // const logImgSrc = () =>
-    //   {
-    //     console.log("Current Image source:",  img.currentSrc + "browser szie:" , window.innerWidth);
-    //   }
+    const img =  document.querySelector(".svg-image");
+    const logImgSrc = () =>
+      {
+        console.log("Current Image source:",  img.currentSrc + "browser szie:" , window.innerWidth);
+      }
 
-    //   window.addEventListener("resize" , logImgSrc);
-    //   logImgSrc();
+      window.addEventListener("resize" , logImgSrc);
+      logImgSrc();
+
+      return () => window.removeEventListener("resize", logImgSrc);
 
   }, []);
 
@@ -34,10 +37,14 @@ const MainHero = ({
   const publicId = image.match(/\/v\d+\/(.+)\.\w+$/)[1];
 
   const cloudinaryImg = cld.image(publicId).format('webp').delivery('q_auto')
- 
-  const smallImg = cld.image(publicId).format('webp').delivery('q_auto').resize(scale().width(600).height(400));
-  const mediumImg = cld.image(publicId).format('webp').delivery('q_auto').resize(scale().width(1000).height(667));
-  const largeImg = cld.image(publicId).format('webp').delivery('q_auto').resize(scale().width(1200).height(800));
+
+  // const smallImg = cld.image(publicId).format('webp').delivery('q_auto').resize(scale().width(600).height(400));
+  // const mediumImg = cld.image(publicId).format('webp').delivery('q_auto').resize(scale().width(1000).height(667));
+  // const largeImg = cld.image(publicId).format('webp').delivery('q_auto').resize(scale().width(1200).height(800));
+
+  // console.log(smallImg.toURL());
+  // console.log(mediumImg.toURL());
+  // console.log(largeImg.toURL());
 
   return (
     <>
@@ -67,33 +74,34 @@ const MainHero = ({
             <div className="hero-section-image" id="box">
               {/* Hero Section Image Starts */}
               <div className="hero-image">
-               <picture>
+               {/* <picture>
                 <source 
                   // media="(max-width: 600px)"
                   srcSet={`${smallImg.toURL()} 1x`}
                   sizes="(max-width: 600px) 90vw"
-                />
+                  />
                 <source
                     // media="(max-width: 768px)"
                     srcSet={`${mediumImg.toURL()} 1.5x`}
                     sizes="(max-width: 768px) 80vw"
-                  />
+                    />
                   <source
                     // media="(max-width: 1200px)"
                     srcSet={`${largeImg.toURL()} 2x`}
                     sizes="(max-width: 1200px) 70vw"
                   />
+               </picture> */}
                 
                <AdvancedImage
                     // src={cloudinaryImg.toURL()}
                     cldImg={cloudinaryImg}
+                    plugins={[responsive({steps: [480, 768, 1024,1200]})]}
                     fetchpriority="high"
                     alt="Cyber-Image"
                     className="svg-image slide-right"
                     width={612}
                     height={408}
                   />
-               </picture>
 
               </div>
             </div>
