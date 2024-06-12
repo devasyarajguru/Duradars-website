6bd05f32e7c33e2954e8ba145c6836b263d94b54
Devasya <devasyarajguru@gmail.com>
2024-06-12 14:16:47 +0530

    update: Removed ScrollContext Provider wrapped inApp Component

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import { BrowserRouter as Router} from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
     <Router>
- 
     <App />
     </Router>
   </React.Fragment>,
